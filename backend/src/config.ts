9734a82e5c73ad10f5c5647481f20f94d0b1ab7a
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-29 18:51:02 +0530

    added mongodb database

---
+++ backend/src/config.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function connectDB() {
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    console.error(`MONGO_URI is not defined in the enviroment variables`);
+    process.exit(1);
+  }
+  try {
+    await mongoose.connect(mongoUri);
+    console.log(`Database Up Successfully ✅ `);
+  } catch (error) {
+    console.error(`Error connecting to database ❌:`, error);
+    process.exit(1);
+  }
+}
