48c2ca0b9183fd8b9fdf1aee29792fe774be28ec
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-30 18:31:04 +0530

    setup routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ConnectDB from "./config";
+import mainrouter from "./routers/index";
 
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT;
+
 app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", mainrouter);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
